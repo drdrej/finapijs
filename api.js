13ee4a7c2220806ca124e8339f5e33c78c2b61a6
drdrej <ask@touchableheroes.com>
2017-01-14 21:24:43 +0100

    upd

---
+++ api.js
@@ -0,0 +1,9 @@
+/**
+ * Created by asiebert on 14.01.17.
+ */
+
+module.exports = {
+  connect  : require( './fints-read-module' ),
+  getAllTX : require( './fints-get-all-tx-module'),
+  stopDialog  : require( './fints-cancel-dialog-module' )
+};
